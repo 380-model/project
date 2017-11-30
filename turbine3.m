183972b516d890fc6937cbb8cf03b74b7ed989ca
Richard Arthurs <rarthurs@sfu.ca>
2017-11-29 16:33:41 -0800

    Added calculation for gas pressure

--- turbine3.m
+++ turbine3.m
@@ -1,13 +1,26 @@
 %% ---------- Added Rectifier Circuit --------------
 close all
+% penstock flow rate, turbine speed, generator speed, shaft torque,
+% generator current, generator voltage, moles of H2, moles of O2
+
 y0 = [0,0,0,0,0,0,0,0]; % initial angular velocity = 0
-[t,y] = ode45(@turbine4, [0 15], y0);
+[t,y] = ode45(@turbine4, [0 150], y0);
+
+%% Calculate Pressure of H2 and O2 for fuel cell
+volume = 20/1000; % 20 L tanks (each gas)
+p_H2 = y(:,7);
+p_O2 = y(:,8);
+p_H2 = p_H2(end) * 8.314 * 298.15 / volume; % pascals
+p_O2 = p_O2(end) * 8.314 * 298.15 / volume; % pascals
+
+%% Do various conversions
 shaft_power = y(:,3) .* y(:,4);
 electrical_power = y(:,5) .* y(:,6);
 
 y(:,2) = y(:,2) / (2*pi) * 60;  % convert to RPM
 y(:,3) = y(:,3) / (2*pi) * 60;  % convert to RPM
 
+%% Plotting 
 figure
 plot(t,y(:,2),t,y(:,3),t,y(:,4),t,y(:,5),t,y(:,6))
 hold on
@@ -47,18 +60,19 @@ fprintf('Average shaft power: %f W\r\n',mean(shaft_power))
 fprintf('Average electrical power: %f W\r\n',mean(electrical_power))
 fprintf('Generator efficiency: %f percent\r\n',mean(electrical_power)/mean(shaft_power)*100)
 
+%% Nonlinear Input Model
 function xprime = turbine4(t,y)
 % Turbine (Pelton Wheel) Parameters
 mass_turb = 10;
-radius_turb = 0.27;
+radius_turb = 0.21;
 J_turb = 0.5 * mass_turb * radius_turb^2; % approximate as a disc
 b_turb = 0.2;
 
 % Generator
-J_gen = 0.005; % less than the turbine
-k_emf = 1.4; % emf constant from DC-540 generator
+J_gen = J_turb/2; % less than the turbine
+k_emf = 1.4;
 L_gen = 0.315;
-R_gen = 0.27; %http://forums.pelicanparts.com/porsche-911-technical-forum/199928-alternator-stator-coil-resistance.html
+R_gen = 0.27; 
 b_gen = 0.35;
 R_load = 40.7;
 C_gen = 0.005;
@@ -94,71 +108,4 @@ xprime(5,1) = (k_emf*y(3)/sqrt(2) - (y(5) - (y(6)-1.4)/R_load)) / L_gen; % induc
 xprime(6,1) = (y(5) - y(6)/R_gen)/C_gen; % output voltage
 xprime(7,1) = y(5) * y(6) * 8.314 * 298.15 /285000 /101300 * 0.0899 /0.00201588; %vol * density / molar mass =  moles of H2
 xprime(8,1) = y(5) * y(6) * 8.314 * 298.15 /285000 /101300 /2 * 1.429 / .03199; %vol * density / molar mass =  moles of O2
-
-% xprime(5,1) = (k_emf*y(3)/sqrt(2) - (y(6)/R_load + y(5))*R_load)/L_gen;
-% xprime(6,1) = (k_emf*y(3)/sqrt(2)/R_gen - y(5))/C_gen;
 end
-
-% %% ---------- Added Penstock Pipe --------------
-% 
-% y0 = [0,0,0,0,0]; % initial angular velocity = 0
-% [t,y] = ode45(@turbine4, [0 5], y0);
-% power = y(:,3) .* y(:,4);
-% y(:,2) = y(:,2) / (2*pi) * 60;  % convert to RPM
-% y(:,3) = y(:,3) / (2*pi) * 60;  % convert to RPM
-% figure
-% plot(t,y)
-% hold on
-% plot(t, power)
-% title('Pelton Wheel Turbine')
-% legend('Penstock Flow Rate (m^3/s)','Turbine RPM','Generator RPM','Shaft Torque (N*m)', 'Output current (A)', 'Output Electrical Power')
-% xlabel('Simulation time (s)')
-% ylabel('Value')
-% 
-% function xprime = turbine4(t,y)
-% % Turbine (Pelton Wheel) Parameters
-% mass_turb = 10;
-% radius_turb = 0.15;
-% J_turb = 0.5 * mass_turb * radius_turb^2; % approximate as a disc
-% b_turb = 0.2;
-% 
-% % Generator
-% J_gen = 0.005; % less than the turbine
-% k_emf = 0.699; % emf constant from DC-540 generator
-% k_gen = 0.999;% motor (generator constant for DC-540 generator)
-% L_gen = 0.315;
-% R_gen = 0.27; %http://forums.pelicanparts.com/porsche-911-technical-forum/199928-alternator-stator-coil-resistance.html
-% b_gen = 0.35;
-% R_load = 30;
-% 
-% % Shaft parameters
-% G = 80E9; % stainless steel
-% d = 1/100; % 1cm
-% l = 0.1; % 10cm
-% % calculate_k = @(G, d, l) -(((pi * d^4)/32)*G)/l;
-% K = -785.3982;%calculate_k(G,d,l);
-% 
-% % Penstock Pipe 
-% head = 50;
-% pipe_diam = 0.0736;
-% pipe_length = 50/sin(45);
-% pipe_area = (pipe_diam/2)^2 * pi;
-% Rf = (128 * 8.9E-4 * pipe_length) / (pi * pipe_diam^4); 
-% If = 2 * 1000 * pipe_length / pipe_area;
-% 
-% % Water jet
-% jet_diam = 0.015;
-% jet_area = pi*(jet_diam/2)^2;
-% beta = jet_diam/pipe_diam;
-% jet_coefficient = 0.97;
-% 
-% jet_velocity = sqrt(2*9.8*(head - (Rf * y(1) / 1000 / 9.8)));
-% 
-% xprime(1,1) = (1000 * 9.8 * head - Rf*y(1) - 0.5*1000*(1-beta^4)*(y(1)/(jet_coefficient*jet_area))^2)/If; % penstock flow rate
-% xprime(2,1) = (2000 * y(1) * radius_turb / J_turb)*(jet_coefficient * jet_velocity - y(2)*radius_turb) - y(2)*b_turb/J_turb - y(4)/J_turb; % turbine speed
-% xprime(3,1) = (y(4) - y(3)*b_gen)/J_gen; % Generator speed
-% xprime(4,1) = K * (-y(2) + y(3));  % shaft torque
-% xprime(5,1) = (1/L_gen) * (k_emf * y(3) - R_gen*y(5) - R_load*y(5)); % Output current
-% 
-% 
-% end
--- turbine3_linear.m
+++ turbine3_linear.m
@@ -1,8 +1,17 @@
 clear all
-% State variables: w_turb, w_gen, T_k, I_gen
+%% Setup Variables
+% Tanks
+volume = 20/1000; % 20 L tanks (each gas)
+
+% Turbine (Pelton Wheel) Parameters
+mass_turb = 10;
+radius_turb = 0.21;
+J_turb = 0.5 * mass_turb * radius_turb^2; % approximate as a disc
+b_turb = 0.2;
+
 % Generator
 J_gen = 0.005; % less than the turbine
-k_emf = 0.699; % emf constant from DC-540 generator
+k_emf = 1.4; % emf constant from DC-540 generator
 L_gen = 0.315;
 R_gen = 0.27; %http://forums.pelicanparts.com/porsche-911-technical-forum/199928-alternator-stator-coil-resistance.html
 b_gen = 0.35;
@@ -35,17 +44,14 @@ radius_turb = 0.15;
 J_turb = 0.5 * mass_turb * radius_turb^2; % approximate as a disc
 b_turb = 0.2;
 
-y0 = [0 0 0];
-
-Q = 5.4/1000;
+%% Setup Simulation
+Q = 5.4/1000; % flow rate. From steady state flow rate determined using nonlinear model. 
 
 a = [((-2*radius_turb^2*Q*1000)/J_turb - b_turb/J_turb) 0 -1/J_turb 0 0;
     0 -b_gen/J_gen 1/J_gen 0 0;
     -K K 0 0 0;
     0 k_emf/sqrt(2)/L_gen 0 -1/(L_gen) 1/(L_gen * R_load)
     0 0 0 1/C_gen -1/(R_gen * C_gen)];
-
-% todo: adjust R_load based on requirements of electrolyzer
 
 b = [2000*radius_turb*Q*jet_coefficient*sqrt(2*9.8*head)/J_turb; 
     0 
@@ -62,9 +68,10 @@ c = [1 0 0 0 0
  
 d = 0;
 
+%% Simulate the System
 sys = ss(a, b, c, d);
 dt = 0.001;
-t = 0:dt:360;
+t = 0:dt:15;
 u = ones(length(t), 1);
 
 simresult = lsim(sys, u, t);
@@ -72,10 +79,15 @@ plot(t,simresult)
 legend('Turbine Shaft Rad/s', 'Generator Shaft Rad/s', 'Shaft Torque (N*m)','Output Current', 'Output Voltage');
 xlabel('Time (s)')
 
-% Calculate volume of H2 produced at each time step
-H2 = simresult(:,5) .* simresult(:,4) * (dt /285000 * 8.314 * 298.15 / 101300 * 0.0899 / 0.00201588); % moles of H2
-O2 = simresult(:,5) .* simresult(:,4) * (dt /285000 * 8.314 * 298.15 /2 /101300 * 1.429 / 0.03199); % moles of H2
+%% Calculate volume of H2 produced at each time step
+moles_H2 = simresult(:,5) .* simresult(:,4) * (dt /285000 * 8.314 * 298.15 / 101300 * 0.0899 / 0.00201588); % moles of H2
+moles_O2 = simresult(:,5) .* simresult(:,4) * (dt /285000 * 8.314 * 298.15 /2 /101300 * 1.429 / 0.03199); % moles of H2
 
-H2 = cumsum(H2);
-fprintf('Total volume of H2 produced in %d seconds: %d m^3\r\n',t(end),H2(end))
-fprintf('Total volume of O2 produced in %d seconds: %d m^3\r\n',t(end),H2(end)/2)
+moles_H2 = cumsum(moles_H2); % integrate
+moles_O2 = cumsum(moles_O2);
+
+p_H2 = moles_H2(end) * 8.314 * 298.15 / volume; % pascals
+p_O2 = moles_O2(end) * 8.314 * 298.15 / volume; % pascals
+
+fprintf('Total moles of H2 produced in %d seconds: %d \r\n',t(end),moles_H2(end))
+fprintf('Total moles of O2 produced in %d seconds: %d \r\n',t(end),moles_O2(end)/2)
